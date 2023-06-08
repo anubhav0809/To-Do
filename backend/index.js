27bc5f50d4c25d3053093c57015391e6e4ba37e4
Anubhav <anubhavsaturday@gmail.com>
2023-06-09 01:14:27 +0530

    index.js file updated

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-
+//use express.json() to get data into json format
 app.use(express.json());
+//Port 
+const PORT = process.env.PORT || 5500;
+
+//use cors
 app.use(cors());
 
-const PORT = process.env.PORT || 5500
+//import routes
+const TodoItemRoute = require('./routes/todoItems');
+
+
+//connect to mongodb ..
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+
+
+app.use('/', TodoItemRoute);
 
 
 
-
-
-app.listen(PORT, ()=> console.log("Server connected"))
+//connect to server
+app.listen(PORT, ()=> console.log("Server connected") );
